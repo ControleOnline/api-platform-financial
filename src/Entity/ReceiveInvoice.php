565f52f20fe39ac53f1a3a492e7d52443643c7a0
leandrogabrielcunha <devmunds@gmail.com>
2023-11-01 18:17:49 -0300

    Update

--- src/Entity/ReceiveInvoice.php
+++ src/Entity/ReceiveInvoice.php
@@ -11,6 +11,9 @@ use ApiPlatform\Metadata\ApiFilter;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+
+use App\Entity\Category;
+use App\Entity\Order;
 /**
  * ReceiveInvoice
  *
