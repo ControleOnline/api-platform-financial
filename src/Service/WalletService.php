0d2f15daeec34086b76ccaac4398241c231b8856
Luiz Kim <luiz.kim@controleonline.com>
2025-01-14 17:12:44 -0300

    Update

---
+++ src/Service/WalletService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ControleOnline\Service;
+
+use ControleOnline\Entity\Invoice;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+use Doctrine\ORM\QueryBuilder;
+
+class WalletService
+{
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private Security $security,
+        private PeopleService $PeopleService
+    ) {}
+
+    public function secutiryFilter(QueryBuilder $queryBuilder, $resourceClass = null, $applyTo = null, $rootAlias = null): void
+    {
+        $this->PeopleService->checkCompany('people', $queryBuilder, $resourceClass, $applyTo, $rootAlias);
+    }
+}
