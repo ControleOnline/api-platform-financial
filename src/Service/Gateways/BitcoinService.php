7fc3c69e7c188263e3041f484d113bac1f96fe46
Luiz Kim <luiz.kim@controleonline.com>
2025-01-30 10:32:26 -0300

    Update

--- src/Service/Gateways/BitcoinService.php
+++ src/Service/Gateways/BitcoinService.php
@@ -3,26 +3,29 @@
 namespace ControleOnline\Service\Gateways;
 
 use ControleOnline\Entity\Invoice;
-use \Endroid\QrCode\Builder\Builder;
-use \Endroid\QrCode\Encoding\Encoding;
-use \Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
 
 class BitcoinService
 {
     public function getBitcoin(Invoice $invoice): array
     {
         $walletAddress = 'BC1QQDMP6P903LNVLQRJ7QWMSGWX7D8YLTQURZDMPL';
-        $amount = $invoice->getPrice(); // Valor em BTC
+        $amount = $invoice->getPrice();
         $payload = "bitcoin:{$walletAddress}?amount={$amount}";
 
-        $qrCode = Builder::create()
-            ->writer(new PngWriter())
-            ->data($payload)
-            ->encoding(new Encoding('UTF-8'))
-            ->size(300)
-            ->build();
+        $qrCode = QrCode::create($payload)
+            ->setEncoding(new Encoding('UTF-8'))
+            ->setSize(300)
+            ->setMargin(10)
+            ->setErrorCorrectionLevel(ErrorCorrectionLevel::High);
+        
+        $writer = new PngWriter();
+        $qrCodeResult = $writer->write($qrCode);
 
-        $encodedImage = base64_encode($qrCode->getString());
+        $encodedImage = base64_encode($qrCodeResult->getString());
 
         return [
             'payload' => $payload,
