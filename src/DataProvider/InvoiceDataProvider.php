ac376d8cb8425fdcf569c3d8593e153113c60e47
Luiz Kim <luiz.kim@controleonline.com>
2025-03-18 19:04:30 -0300

    Update

--- src/DataProvider/InvoiceDataProvider.php
+++ src/DataProvider/InvoiceDataProvider.php
@@ -70,7 +70,7 @@ class InvoiceDataProvider implements ProviderInterface
             if (!isset($data['wallet'][$dWalletId]))
                 $data['wallet'][$dWalletId] = [
                     'wallet' => $row['dwallet'],
-                    'inflow-wallet' => $row['owallet'],
+                    'inflow-wallet' => 'xxx' . $row['owallet'],
                     'payment' => [],
                     'total' => 0.0,
                 ];
