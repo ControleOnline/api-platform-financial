dd2ea24e09005e4c487502bcdc8d23141fcc057a
Luiz Kim <luiz.kim@controleonline.com>
2025-03-18 08:13:42 -0300

    Update

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -298,7 +298,7 @@ class Invoice
 
     /**
      * @ORM\ManyToOne(targetEntity="ControleOnline\Entity\User")
-     * @ORM\JoinColumn(nullable=false)
+     * @ORM\JoinColumn(nullable=true)
      * @Groups({"invoice:read","invoice_details:read","logistic:read","invoice:write","order_invoice:write"})
      */
     #[ApiFilter(filterClass: SearchFilter::class, properties: ['user' => 'exact'])]
