913f10376e18f245d633e735f81d751971386ece
Luiz Kim <luiz.kim@controleonline.com>
2025-04-03 03:10:52 -0300

    Update

--- src/Controller/GetCashRegisterController.php
+++ src/Controller/GetCashRegisterController.php
@@ -10,23 +10,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-
 class GetCashRegisterController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
         private CashRegisterService $cashRegister
-
     ) {}
 
     public function __invoke(Request $request): JsonResponse
     {
-        $data = json_decode($request->getContent(), true);
-        $device = $this->entityManager->getRepository(Device::class)->find($data['device']);
-        $company = $this->entityManager->getRepository(People::class)->find($data['company']);
+        $deviceId = $request->query->get('device');
+        $companyId = $request->query->get('company');
 
-        $printData = $this->cashRegister->generateData($device, $company);
+        $device = $this->entityManager->getRepository(Device::class)->find($deviceId);
+        $company = $this->entityManager->getRepository(People::class)->find($companyId);
 
-        return new JsonResponse($printData);
+        $data = $this->cashRegister->generateData($device, $company);
+
+        return new JsonResponse($data);
     }
 }
