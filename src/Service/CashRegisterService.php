4bd85197e86ea4815d0d7f7b832d6b14162f09b3
Luiz Kim <luiz.kim@controleonline.com>
2025-04-13 17:32:16 -0300

    Update

--- src/Service/CashRegisterService.php
+++ src/Service/CashRegisterService.php
@@ -6,6 +6,7 @@ use ControleOnline\Entity\Device;
 use ControleOnline\Entity\OrderProduct;
 use ControleOnline\Entity\People;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 class CashRegisterService
 {
@@ -19,31 +20,47 @@ class CashRegisterService
 
     public function generateData(Device $device, People $provider)
     {
-        $orderProductRepository = $this->entityManager->getRepository(OrderProduct::class);
+        $rsm = new ResultSetMapping();
+        $rsm->addScalarResult('product_name', 'product_name');
+        $rsm->addScalarResult('product_description', 'product_description');
+        $rsm->addScalarResult('product_sku', 'product_sku');
+        $rsm->addScalarResult('quantity', 'quantity');
+        $rsm->addScalarResult('order_product_price', 'order_product_price');
+        $rsm->addScalarResult('order_product_total', 'order_product_total');
 
-        $queryBuilder = $orderProductRepository->createQueryBuilder('op')
-            ->select('DISTINCT p.product AS product_name')
-            ->addSelect('p.description AS product_description')
-            ->addSelect('p.sku AS product_sku')
-            ->addSelect('SUM(op.quantity) AS quantity')
-            ->addSelect('MIN(op.price) AS order_product_price')
-            ->addSelect('SUM(op.total) AS order_product_total')
-            ->join('op.product', 'p')
-            ->join('op.order', 'o')
-            ->join('o.invoice', 'oi')
-            ->join('oi.invoice', 'i')
-            ->join('i.device', 'd')
-            ->andWhere('d.id = :device')
-            ->andWhere('o.provider = :provider')
-            ->andWhere('i.receiver = :provider')
-            ->andWhere('p.type IN(:type)')
-            ->groupBy('p.id')
-            ->addGroupBy('p.product')
-            ->addGroupBy('p.description')
-            ->addGroupBy('p.sku')
-            ->orderBy('p.product', 'ASC');
+        $sql = '
+            SELECT 
+                p.product AS product_name,
+                p.description AS product_description,
+                p.sku AS product_sku,
+                SUM(sub.quantity) AS quantity,
+                MIN(sub.price) AS order_product_price,
+                SUM(sub.total) AS order_product_total
+            FROM (
+                SELECT DISTINCT
+                    op.id,
+                    op.quantity,
+                    op.price,
+                    op.total,
+                    op.product_id
+                FROM order_product op
+                INNER JOIN orders o ON op.order_id = o.id
+                INNER JOIN order_invoice oi ON o.id = oi.order_id
+                INNER JOIN invoice i ON oi.invoice_id = i.id
+                INNER JOIN device d ON i.device_id = d.id
+                WHERE d.id = :device
+                    AND o.provider_id = :provider
+                    AND i.receiver_id = :provider
+            ) sub
+            INNER JOIN product p ON sub.product_id = p.id
+            WHERE p.type IN (:type)
+            GROUP BY p.id, p.product, p.description, p.sku
+            ORDER BY p.product ASC
+        ';
 
-        $queryBuilder
+        $query = $this->entityManager->createNativeQuery($sql, $rsm);
+
+        $query
             ->setParameter('type', ['product', 'custom', 'manufactured'])
             ->setParameter('device', $device->getId())
             ->setParameter('provider', $provider->getId());
@@ -54,17 +71,45 @@ class CashRegisterService
         )->getConfigs(true);
 
         if ($deviceConfig && isset($deviceConfig['cash-wallet-open-id'])) {
-            $queryBuilder->andWhere('i.id > :minId')
+            $sql = str_replace(
+                'WHERE d.id = :device',
+                'WHERE d.id = :device AND i.id > :minId',
+                $sql
+            );
+            $query = $this->entityManager->createNativeQuery($sql, $rsm);
+            $query
+                ->setParameter('type', ['product', 'custom', 'manufactured'])
+                ->setParameter('device', $device->getId())
+                ->setParameter('provider', $provider->getId())
                 ->setParameter('minId', $deviceConfig['cash-wallet-open-id']);
         }
 
         if ($deviceConfig && isset($deviceConfig['cash-wallet-closed-id']) && $deviceConfig['cash-wallet-closed-id'] > 0) {
-            $queryBuilder->andWhere('i.id <= :maxId')
+            $sql = str_replace(
+                'WHERE d.id = :device',
+                'WHERE d.id = :device AND i.id <= :maxId',
+                $sql
+            );
+            if (isset($deviceConfig['cash-wallet-open-id'])) {
+                $sql = str_replace(
+                    'AND i.id > :minId',
+                    'AND i.id > :minId AND i.id <= :maxId',
+                    $sql
+                );
+            }
+            $query = $this->entityManager->createNativeQuery($sql, $rsm);
+            $query
+                ->setParameter('type', ['product', 'custom', 'manufactured'])
+                ->setParameter('device', $device->getId())
+                ->setParameter('provider', $provider->getId())
                 ->setParameter('maxId', $deviceConfig['cash-wallet-closed-id']);
+            if (isset($deviceConfig['cash-wallet-open-id'])) {
+                $query->setParameter('minId', $deviceConfig['cash-wallet-open-id']);
+            }
         }
 
-        error_log($queryBuilder->getQuery()->getSQL());
-        return $queryBuilder->getQuery()->getArrayResult();
+        error_log($query->getSQL());
+        return $query->getArrayResult();
     }
 
     public function generatePrintData(Device $device, $provider, string $printType, string $deviceType)
--- src/Service/InFlowService.php
+++ src/Service/InFlowService.php
@@ -6,6 +6,7 @@ use ControleOnline\Entity\Device;
 use ControleOnline\Entity\Invoice;
 use ControleOnline\Entity\People;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface as Security;
 
 class InFlowService
@@ -24,28 +25,108 @@ class InFlowService
     public function getPayments($filters): array
     {
         $this->filters = $filters;
-        $this->createBaseQuery();
-        $results = $this->qb->getQuery()->getResult();
-        error_log($this->qb->getQuery()->getSQL());
+
+        $rsm = new ResultSetMapping();
+        $rsm->addScalarResult('totalPrice', 'totalPrice');
+        $rsm->addScalarResult('dwalletId', 'dwalletId');
+        $rsm->addScalarResult('dwallet', 'dwallet');
+        $rsm->addScalarResult('owalletId', 'owalletId');
+        $rsm->addScalarResult('owallet', 'owallet');
+        $rsm->addScalarResult('paymentTypeId', 'paymentTypeId');
+        $rsm->addScalarResult('paymentType', 'paymentType');
+
+        $sql = '
+            SELECT 
+                SUM(sub.price) AS totalPrice,
+                sub.dwallet_id AS dwalletId,
+                sub.dwallet AS dwallet,
+                sub.owallet_id AS owalletId,
+                sub.owallet AS owallet,
+                sub.payment_type_id AS paymentTypeId,
+                sub.payment_type AS paymentType
+            FROM (
+                SELECT DISTINCT
+                    i.id,
+                    i.price,
+                    dw.id AS dwallet_id,
+                    dw.wallet AS dwallet,
+                    ow.id AS owallet_id,
+                    ow.wallet AS owallet,
+                    pt.id AS payment_type_id,
+                    pt.payment_type AS payment_type
+                FROM invoice i
+                INNER JOIN wallet dw ON i.destination_wallet_id = dw.id
+                INNER JOIN payment_type pt ON i.payment_type_id = pt.id
+                LEFT JOIN wallet ow ON i.source_wallet_id = ow.id
+                WHERE 1=1
+        ';
+
+        if (isset($this->filters['receiver'])) {
+            $sql .= ' AND i.receiver_id = :receiver';
+        }
+
+        if (isset($this->filters['device.device'])) {
+            $sql .= ' AND i.device_id IN (SELECT d.id FROM device d WHERE d.device = :device)';
+        }
+
+        if (isset($this->filters['device.device']) && isset($this->filters['receiver'])) {
+            $device = $this->entityManager->getRepository(Device::class)->findOneBy(['device' => $this->filters['device.device']]);
+            $people = $this->entityManager->getRepository(People::class)->find($this->filters['receiver']);
+            
+            if ($device && $people) {
+                $device_config = $this->deviceService->discoveryDeviceConfig(
+                    $device,
+                    $people
+                )->getConfigs(true);
+
+                if ($device_config && isset($device_config['cash-wallet-open-id'])) {
+                    $sql .= ' AND i.id > :idGt';
+                }
+
+                if ($device_config && isset($device_config['cash-wallet-closed-id']) && $device_config['cash-wallet-closed-id'] > 0) {
+                    $sql .= ' AND i.id <= :idLt';
+                }
+            }
+        }
+
+        $sql .= '
+            ) sub
+            GROUP BY sub.dwallet_id, sub.payment_type_id, sub.owallet_id
+        ';
+
+        $query = $this->entityManager->createNativeQuery($sql, $rsm);
+
+        if (isset($this->filters['receiver'])) {
+            $query->setParameter('receiver', $this->filters['receiver']);
+        }
+
+        if (isset($this->filters['device.device'])) {
+            $query->setParameter('device', $this->filters['device.device']);
+        }
+
+        if (isset($this->filters['device.device']) && isset($this->filters['receiver'])) {
+            $device = $this->entityManager->getRepository(Device::class)->findOneBy(['device' => $this->filters['device.device']]);
+            $people = $this->entityManager->getRepository(People::class)->find($this->filters['receiver']);
+            
+            if ($device && $people) {
+                $device_config = $this->deviceService->discoveryDeviceConfig(
+                    $device,
+                    $people
+                )->getConfigs(true);
+
+                if ($device_config && isset($device_config['cash-wallet-open-id'])) {
+                    $query->setParameter('idGt', $device_config['cash-wallet-open-id']);
+                }
+
+                if ($device_config && isset($device_config['cash-wallet-closed-id']) && $device_config['cash-wallet-closed-id'] > 0) {
+                    $query->setParameter('idLt', $device_config['cash-wallet-closed-id']);
+                }
+            }
+        }
+
+        error_log($query->getSQL());
+        $results = $query->getArrayResult();
         return $this->getResult($results);
-    }
-
-    private function createBaseQuery()
-    {
-        $this->qb = $this->entityManager->createQueryBuilder()
-            ->select('SUM(DISTINCT i.price) as totalPrice')
-            ->addSelect('dw.id as dwalletId', 'dw.wallet as dwallet')
-            ->addSelect('ow.id as owalletId', 'ow.wallet as owallet')
-            ->addSelect('pt.id as paymentTypeId', 'pt.paymentType')
-            ->from(Invoice::class, 'i')
-            ->join('i.destinationWallet', 'dw')
-            ->join('i.paymentType', 'pt')
-            ->leftJoin('i.sourceWallet', 'ow')
-            ->groupBy('dw.id')
-            ->addGroupBy('pt.id')
-            ->addGroupBy('ow.id');
-
-        $this->applyCommonFilters();
     }
 
     private function getResult($results): array
@@ -98,54 +179,5 @@ class InFlowService
         }
 
         return $data;
-    }
-
-    private function applyCommonFilters(): void
-    {
-        $this->applyDeviceFilter();
-        $this->applyCashRegisterFilter();
-        $this->applyReceiverFilter();
-    }
-
-    private function applyReceiverFilter(): void
-    {
-        if (isset($this->filters['receiver'])) {
-            $this->qb->andWhere('i.receiver = :receiver')
-                ->setParameter('receiver', $this->filters['receiver']);
-        }
-    }
-
-    private function applyDeviceFilter(): void
-    {
-        if (isset($this->filters['device.device'])) {
-            $this->qb->join('i.device', 'd')
-                ->andWhere('d.device = :device')
-                ->setParameter('device', $this->filters['device.device']);
-        }
-    }
-
-    private function applyCashRegisterFilter(): void
-    {
-        if (isset($this->filters['device.device']) && isset($this->filters['receiver'])) {
-            $device = $this->entityManager->getRepository(Device::class)->findOneBy(['device' => $this->filters['device.device']]);
-            $people = $this->entityManager->getRepository(People::class)->find($this->filters['receiver']);
-            
-            if ($device && $people) {
-                $device_config = $this->deviceService->discoveryDeviceConfig(
-                    $device,
-                    $people
-                )->getConfigs(true);
-
-                if ($device_config && isset($device_config['cash-wallet-open-id'])) {
-                    $this->qb->andWhere('i.id > :idGt')
-                        ->setParameter('idGt', $device_config['cash-wallet-open-id']);
-                }
-
-                if ($device_config && isset($device_config['cash-wallet-closed-id']) && $device_config['cash-wallet-closed-id'] > 0) {
-                    $this->qb->andWhere('i.id <= :idLt')
-                        ->setParameter('idLt', $device_config['cash-wallet-closed-id']);
-                }
-            }
-        }
     }
 }
