8b5c7ea3c1e80e7424d0d8063051fcd9a6cf8c34
Luiz Kim <luiz.kim@controleonline.com>
2025-04-17 13:51:06 -0300

    Update

--- src/Service/InFlowService.php
+++ src/Service/InFlowService.php
@@ -80,7 +80,8 @@ class InFlowService
                 (';
         $sql .= $this->getSubquery($deviceConfig);
         $sql .= ') sub
-            JOIN invoice i ON i.id = sub.invoice_id
+            JOIN order_invoice oi ON oi.order_id = sub.id
+            JOIN invoice i ON i.id = oi.invoice_id
             JOIN wallet dw ON i.destination_wallet_id = dw.id
             JOIN payment_type pt ON i.payment_type_id = pt.id
             LEFT JOIN wallet ow ON i.source_wallet_id = ow.id
