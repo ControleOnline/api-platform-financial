921dfbb2a2c7c2cbc68dc37bec779f6460b435e1
Alexandre de Silva Cunha <alemac@mac.com>
2026-07-29 18:51:50 -0300

    again…

--- migrations/Version20260729185000.php
+++ migrations/Version20260729185000.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations\Financial;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260729170000 extends AbstractMigration
+final class Version20260729185000 extends AbstractMigration
 {
     public function getDescription(): string
     {
